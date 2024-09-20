394fe819e46530d564ed01f048baee72a54fe75e
lamle2808 <lamle28082001@gmail.com>
2024-09-20 23:27:56 +0700

    first create entity

---
+++ src/main/java/com/example/kltn/entity/Account.java
@@ -0,0 +1,24 @@
+package com.example.kltn.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Table(name = "accounts")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String userName;
+    private String passWord;
+    private String userRole;
+    private String image;
+}
